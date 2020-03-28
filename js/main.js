45c05d10b2f4e58123ae4069bbfd15ae537f466a
maxi117 <maxisaias2009@gmail.com>
2020-03-27 23:28:03 -0300

    animacion beta

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function() {
+   $('.corazon').click(function() {
+   	$('.fa-heart').toggleClass('pulso');
+   });
+
+    $('.corazon').click(function() {
+   	$('.gradient').removeClass('gradient', {duration:500});
+
+   });
+ });
+
+$(document).ready(function() {
+   $('.corazon').click(function() {
+   	$('.mujer_2').hide(400);
+   });
+
+   	 $('.corazon').click(function() {
+   	$('.mujer_1').show(400);
+   });
+
+ });
+
+
+
+
