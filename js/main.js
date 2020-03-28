96c4d08995defe2c554dab5edb92fa8368e8609a
maxi117 <maxisaias2009@gmail.com>
2020-03-28 14:56:23 -0300

    final

--- js/main.js
+++ js/main.js
@@ -2,24 +2,13 @@
 $(document).ready(function() {
    $('.corazon').click(function() {
    	$('.fa-heart').toggleClass('pulso');
-   });
-
-    $('.corazon').click(function() {
-   	$('.gradient').removeClass('gradient', {duration:500});
-
+      $('.gradient').removeClass('gradient', {duration:500});
+      $('.mujer_2').hide(400);
+      $('.mujer_1').show(400);
    });
  });
 
-$(document).ready(function() {
-   $('.corazon').click(function() {
-   	$('.mujer_2').hide(400);
-   });
 
-   	 $('.corazon').click(function() {
-   	$('.mujer_1').show(400);
-   });
-
- });
 
 
 
